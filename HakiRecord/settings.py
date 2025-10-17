c6fb6ce2937a31da2773367401a4ddff1ef75be1
unknown <oliverstancylugala@gmail.com>
2025-10-17 11:44:03 +0300

    deploy

--- HakiRecord/settings.py
+++ HakiRecord/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
