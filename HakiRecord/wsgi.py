04d079b6d730daa656999d4884b0a7a1bf152f1c
unknown <oliverstancylugala@gmail.com>
2025-10-19 15:30:02 +0300

    Add debug print for PORT in wsgi.py

--- HakiRecord/wsgi.py
+++ HakiRecord/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+print(f"PORT: {os.environ.get('PORT', 'Not set')}")
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HakiRecord.settings')
 
 application = get_wsgi_application()
+
+
+
+# Keep the existing code below this
